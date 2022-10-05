d3936042150b6dbf5068589bac0c63633633b983
Vargas <renier1989@gmail.com>
2022-10-05 17:19:15 -0500

    up fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode:"class",
+  content: ["./index.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'Montserrat' : ['Montserrat', 'san-serif']
+      }
+    },
   },
   plugins: [],
 }

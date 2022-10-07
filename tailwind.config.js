5c645ef5721845caf82d0143bc371a28fc5197a4
Vargas <renier1989@gmail.com>
2022-10-07 17:23:13 -0500

    ajustes dark mode falta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:"class",
+  darkMode : 'class', 
   content: ["./index.{html,js}"],
   theme: {
     extend: {
+      textColor : {
+        'primary' : '#CC2D4A',
+        'secondary' : '#8FA206',
+        'terciary' : '#61AEC9',
+      },
       fontFamily : {
         'Montserrat' : ['Montserrat', 'san-serif']
       }
